c358a150a1d5abfc1858e7aa6c770bd4cf005b72
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-09-20 22:18:32 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range((1, 10):
-        if i < j:
-		if i == 8:
-            print("{}{}".format(i, j))
+    for j in range((i+1), 10):
+        if (i is not 8) or (j is not 9):
+            print("{}{}, ".format(i, j), end="")
         else:
-            print("{}{},".format(i, j), end=" ")
+            print("{}{}".format(i, j))
