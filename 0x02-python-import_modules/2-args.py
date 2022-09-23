298870db3a6e08b5c8b0ceddcfb7c44ea7688a97
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-09-23 07:34:28 +0000

    Fast alphabet

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
-    size = len(userin)
-    print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg))
+from sys import argv
+if __name__ == '__main__':
+    ac = len(argv) - 1
+
+    if ac == 0:
+        print('{} arguments.'.format(ac))
+    else:
+        if ac == 1:
+            print('{} argument:'.format(ac))
+        else:
+            print('{} arguments:'.format(ac))
+        for i in range(1, ac + 1):
+            print('{}: {}'.format(i, argv[i]))
