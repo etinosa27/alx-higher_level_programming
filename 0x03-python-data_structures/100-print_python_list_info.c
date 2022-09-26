67e5c736f3f29c06f4a675532c4ae9afee9369b8
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-09-26 11:11:58 +0000

    CPython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,11 @@
 #include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_python_list_info - C function that prints some basic info 
+ * about Python lists
+ * @p: python list
+ */
 
 void print_python_list_info(PyObject *p)
 {
