a44bb95183314cc9d50bcd292618c36a11a956e1
etinosa27 <etinosa27@gmail.com>
2022-10-12 22:33:58 +0000

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -15,23 +15,23 @@ class Node:
         self.next_node = next_node
 
 	 @property
-    def data(self):
+	def data(self):
         """Get/set the data of the Node."""
         return (self.__data)
 
-    @data.setter
-    def data(self, value):
+	@data.setter
+	def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
-    @property
-    def next_node(self):
+	@property
+	def next_node(self):
         """Get/set the next_node of the Node."""
         return (self.__next_node)
 
-    @next_node.setter
-    def next_node(self, value):
+	@next_node.setter
+	def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
@@ -40,11 +40,11 @@ class Node:
 	class SinglyLinkedList:
     """Represent a singly-linked list."""
 
-    def __init__(self):
+	def __init__(self):
 	"""Initalize a new SinglyLinkedList."""
 	self.__head = None
 
-    def sorted_insert(self, value):
+	def sorted_insert(self, value):
         """Insert a new Node to the SinglyLinkedList.
         The node is inserted into the list at the correct
         ordered numerical position.
--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0, position=(0, 0)):
+	def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
             size (int): The size of the new square.
@@ -14,26 +14,26 @@ class Square:
         self.size = size
         self.position = position
 
-    @property
-    def size(self):
+	@property
+	def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
 
-    @size.setter
-    def size(self, value):
+	@size.setter
+	def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+	self.__size = value
 
-	 @property
-    def position(self):
+	@property
+	def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
 
-    @position.setter
-    def position(self, value):
+	@position.setter
+	def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -41,11 +41,11 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    def area(self):
+	def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
 
-    def my_print(self):
+	def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
             print("")
@@ -57,7 +57,7 @@ class Square:
             [print("#", end="") for k in range(0, self.__size)]
             print("")
 
-    def __str__(self):
+	def __str__(self):
         """Define the print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
