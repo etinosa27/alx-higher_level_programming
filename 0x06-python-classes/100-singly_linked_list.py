ead2be8c2e43cdb3d0727895451804231070d5ac
etinosa27 <etinosa27@gmail.com>
2022-10-12 23:06:57 +0000

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,7 +5,7 @@
 class Node:
     """Represent a node in a singly-linked list."""
 
-	def __init__(self, data, next_node=None):
+    def __init__(self, data, next_node=None):
         """Initialize a new Node.
         Args:
             data (int): The data of the new Node.
@@ -14,37 +14,37 @@ class Node:
         self.data = data
         self.next_node = next_node
 
-	 @property
-	def data(self):
+    @property
+    def data(self):
         """Get/set the data of the Node."""
         return (self.__data)
 
-	@data.setter
-	def data(self, value):
+    @data.setter
+    def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
-	@property
-	def next_node(self):
+    @property
+    def next_node(self):
         """Get/set the next_node of the Node."""
         return (self.__next_node)
 
-	@next_node.setter
-	def next_node(self, value):
+    @next_node.setter
+    def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
-	class SinglyLinkedList:
+class SinglyLinkedList:
     """Represent a singly-linked list."""
 
-	def __init__(self):
-	"""Initalize a new SinglyLinkedList."""
-	self.__head = None
+    def __init__(self):
+        """Initalize a new SinglyLinkedList."""
+        self.__head = None
 
-	def sorted_insert(self, value):
+    def sorted_insert(self, value):
         """Insert a new Node to the SinglyLinkedList.
         The node is inserted into the list at the correct
         ordered numerical position.
@@ -53,7 +53,7 @@ class Node:
         """
 	new = Node(value)
         if self.__head is None:
-	new.next_node = None
+            new.next_node = None
             self.__head = new
         elif self.__head.data > value:
             new.next_node = self.__head
@@ -66,7 +66,7 @@ class Node:
             new.next_node = tmp.next_node
             tmp.next_node = new
 
-	def __str__(self):
+    def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
         values = []
         tmp = self.__head
