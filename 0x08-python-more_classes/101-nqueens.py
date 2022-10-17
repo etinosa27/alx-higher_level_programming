b23be1593aed3eb20c9a2228db36ea7e3e2f0b8f
etinosa27 <etinosa27@gmail.com>
2022-10-17 20:53:26 +0000

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -16,7 +16,7 @@ import sys
 
 
     def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+	"""Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
@@ -24,14 +24,14 @@ import sys
 
 
     def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
+	"""Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
 
 
     def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
+	"""Return the list of lists representation of a solved chessboard."""
     solution = []
     for r in range(len(board)):
         for c in range(len(board)):
@@ -41,7 +41,7 @@ import sys
     return (solution)
 
     def xout(board, row, col):
-    """X out spots on a chessboard.
+	"""X out spots on a chessboard.
     All spots where non-attacking queens can no
     longer be played are X-ed out.
     Args:
@@ -92,9 +92,9 @@ import sys
 
 
     def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
-    Args:
-        board (list): The current working chessboard.
+    	"""Recursively solve an N-queens puzzle.
+	Args:
+	board (list): The current working chessboard.
         row (int): The current working row.
         queens (int): The current number of placed queens.
         solutions (list): A list of lists of solutions.
@@ -117,13 +117,13 @@ import sys
 
 
     if __name__ == "__main__":
-    if len(sys.argv) != 2:
+	if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+	if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+	if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
 
