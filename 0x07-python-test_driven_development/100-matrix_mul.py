b1d6a763d4c5f70acde41d5ecb77afd030b12c70
etinosa27 <etinosa27@gmail.com>
2022-10-18 15:10:03 +0000

    Max integer - Unittest

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,27 @@
+eachrow) != len(m_b):
+            raise ValueError("m_a and m_b can't be multiplied")
+#!/usr/bin/python3
+"""
+Module 100-matrix_mul
+Contains method that does matrix multiplication
+Requires same size lists/rows of ints or floats
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Returns resulting matrix multiplication
+    """
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("{} must be a list".format
+                        ("m_a" if not isinstance(m_a, list) else "m_b"))
+
+    if len(m_a) == 0 or len(m_b) == 0 or m_a == [[]] or m_b == [[]]:
+        raise ValueError("{} can't be empty".format
+                         ("m_a" if len(m_a) == 0 else "m_b"))
+
+    for eachrow in m_a:
+        for n in eachrow:
+            if not isinstance(n, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+        if len(eachrow) != l
